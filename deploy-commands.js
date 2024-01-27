1492d0c88786f2318eb8f7c94def1cf50dbef6e8
Aeron-Emory <57010069+Aeron-Emory@users.noreply.github.com>
2024-01-26 20:38:20 -0500

    Update deploy-commands.js
    added code

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,29 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require('dotenv').config();
 
+const commands = [{
+  name: 'start',
+  description: 'Starts the Minecraft server'
+}];
+
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+// Replace with your Discord bot's client ID and your server (guild) ID
+const clientId = 'your_discord_client_id';
+const guildId = 'your_discord_guild_id';
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})();
